2d3ab035a43cd621640f17b1a6c353943bd9cf11
PetarKitanovic <kitanovic.petar00@gmail.com>
2020-08-11 12:02:10 +0200

    UI layout

---
+++ app/src/main/java/petarkitanovic/androidkurs/k7tech/service/Common.java
@@ -0,0 +1,7 @@
+package petarkitanovic.androidkurs.k7tech.service;
+
+public class Common {
+    static final String APIKEY = "318f437d83e41a583fbaeceb311df5d9";
+    static final String WEATHERURL = "https://api.openweathermap.org/data/2.5/onecall";
+
+}
